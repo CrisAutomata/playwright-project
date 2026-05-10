58f621063d2ba90573a8320a848801beb52cfe1b
Cris Nguyen <camnm.youthplus@gmail.com>
2026-05-10 23:11:58 +0700

    tweak design 2

--- scripts/summary.mjs
+++ scripts/summary.mjs
@@ -158,14 +158,10 @@ async function generateSummary() {
     // Title
     core.summary.addHeading(`MOA Test Summary: ${health}`, 1);
     core.summary.addRaw('\n\n');
-    core.summary.addCodeBlock(
-        `Passed: ${passPct}% 🟩 ${compare} (${health})
-        Failed: ${failPct}% 🟥
-        Skipped: ${skipPct}% 🟨
-        `
-        , 'javascript')
-    core.summary.addRaw('\n');
-    core.summary.addCodeBlock(output).write();
+    core.summary.addCodeBlock(`Passed: ${passPct}% 🟩 ${compare} (${health})`, 'javascript');
+    core.summary.addCodeBlock(`Failed: ${failPct}% 🟥`, 'javascript');
+    core.summary.addCodeBlock(`Skipped: ${skipPct}% 🟨`, 'javascript');
+    core.summary.addRaw(output).write();
     core.summary.addRaw('\n');
 
 
